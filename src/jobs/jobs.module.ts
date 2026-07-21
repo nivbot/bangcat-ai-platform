a7c92d6deb786e6cdbca7f6835a53fb117f8d512
niv yan <nivmight@gmail.com>
2026-07-22 01:05:36 +0800

    fix: keep queue consumers out of API process

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JobDispatchService } from './job-dispatch.service.js';
 import { JobsController } from './jobs.controller.js';
-import { TopicAnalysisProcessor } from './topic-analysis.processor.js';
 
 @Module({
   controllers: [JobsController],
-  providers: [JobDispatchService, TopicAnalysisProcessor],
+  providers: [JobDispatchService],
   exports: [JobDispatchService],
 })
 export class JobsModule {}
